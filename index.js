b11fe26805a2dd32db8c56f54cdfd2b64575aafe
ShanteL Gray <Shantelgray24@gmail.com>
2025-02-26 11:57:50 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+const employee = {
+ name: "Shantel",
+ streetAddress: "4186 Shoreside Circle",
+}; 
+function updateEmployeeWithKeyAndValue() {
+    return { name: `Sam`, streetAddress: `11 Broadway` }
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+function deleteFromEmployeeByKey() {
+    let newEmployee = {...employee };
+    delete newEmployee.name;
+    return newEmployee;
+};
+function destructivelyDeleteFromEmployeeByKey() {
+    let newEmployee = employee;
+    delete newEmployee.name;
+    return newEmployee;
+};

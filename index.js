445799065ed3655fd9063bd7e52bb904481956ef
Shantel <Shantelgray24@gmail.com>
2025-05-11 18:00:56 -0400

    Creating objects with the right operating elements

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+  name: "Lopez",
+  streetAddress: "14208 Stark Way",
+};
+
+function updateEmployeeWithKeyAndValue() {
+  return { name: "Sam", streetAddress: '11 Broadway' }
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+};
+function deleteFromEmployeeByKey(employee, key) {
+  let newEmployee = {...employee };
+  delete newEmployee.name;
+  return newEmployee;
+};
+function destructivelyDeleteFromEmployeeByKey() {
+  let newEmployee = employee;
+  delete newEmployee.name;
+  return newEmployee;
+};
